27afc01535cd1cc391fdec878fc911dff132b446
Patrick Mulder <mulder.patrick@gmail.com>
2013-04-25 19:56:04 +0200

    fix navbar for devise routes

--- app/assets/javascripts/views/items/logout_navbar.js
+++ app/assets/javascripts/views/items/logout_navbar.js
@@ -6,30 +6,18 @@ MA.Views.Items.LogoutNavbar = Backbone.Marionette.ItemView.extend({
   model: MA.currentUser,
 
   events: {
+    'click a.brand': 'showMain',
 	'click #addmovie': 'showNewMovie',
+  },
+
+  showMain: function(ev) {
+	ev.preventDefault();
+	MA.vent.trigger('authentication:logged_in', ev);
   },
 
   showNewMovie: function(ev) {
 	ev.preventDefault();
 	MA.vent.trigger('menu:new_movie', ev);
-  },
-
-  logout: function(){
-	$.ajax({
-	 method: 'DELETE',
-	 url: '/api/users/sign_out.json',
-	 data: {user: this.model.attributes},
-	 success: function(response) { 
-	     MA.currentUser = null;
-	     MA.vent.trigger("authentication:logged_out");
-	  },
-	  error: function(response) { 
-	     console.log('...'); 
-	     var result = $.parseJSON(response.responseText);
-	     console.log(result);
-	     el.find('input.btn-primary').prop('value', 'Sign up');
-	 }
-	});
   },
 
   onRender: function() {
